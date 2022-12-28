303023914daddccd0fd86e8ad244ca6f40a3988d
Kirill <40721919+Perfecy@users.noreply.github.com>
2022-12-29 01:45:57 +0300

    added fact weather forecast

--- crawler/scheduler_grip.py
+++ crawler/scheduler_grip.py
@@ -7,6 +7,7 @@ from pymongo import MongoClient
 from geopy.geocoders import Nominatim
 import requests
 from db import db, merged_data, unmerged_data
+from collections import Counter
 
 geolocator = Nominatim(user_agent="my_app")
 
@@ -40,6 +41,67 @@ def get_openmeteo(lat: float, lon: float):
     pass
 
 
+def get_unmerged_temp(lat: float, lon: float) -> dict:
+    directions = [
+        "N",
+        "NNE",
+        "NE",
+        "ENE",
+        "E",
+        "ESE",
+        "SE",
+        "SSE",
+        "S",
+        "SSW",
+        "SW",
+        "WSW",
+        "W",
+        "WNW",
+        "NW",
+        "NNW",
+    ]
+    res_yan = get_yandex_weather(lat, lon)
+    res_open = get_open_weather(lat, lon)
+    res_vis = get_vis_crossing_weather(lat, lon)
+    unmerged = {
+        "Yandex": {
+            "temp": res_yan["fact"]["temp"],
+            "condition": res_yan["fact"]["condition"],
+            "pressure_mm": res_yan["fact"]["pressure_mm"],
+            "pressure_pa": res_yan["fact"]["pressure_pa"],
+            "humidity": res_yan["fact"]["humidity"],
+            "feels_like": res_yan["fact"]["feels_like"],
+            "wind_dir": res_yan["fact"]["wind_dir"].upper(),
+            "wind_speed": res_yan["fact"]["wind_speed"],
+        },
+        "OpenWeather": {
+            "temp": (res_open["main"]["temp_min"] + res_open["main"]["temp_max"]) / 2,
+            "condition": res_open["weather"][0]["main"],
+            "pressure_mm": res_open["main"]["pressure"] * 0.007501,
+            "pressure_pa": res_open["main"]["pressure"],
+            "humidity": res_open["main"]["humidity"],
+            "feels_like": res_open["main"]["feels_like"],
+            "wind_dir": directions[
+                int(round(float(res_open["wind"]["deg"]) / 22.5)) % 16
+            ],
+            "wind_speed": res_open["wind"]["speed"],
+        },
+        "VisualCrossing": {
+            "temp": (res_vis["days"][0]["tempmax"] + res_vis["days"][0]["tempmin"]) / 2,
+            "condition": "smth",
+            "pressure_mm": res_vis["days"][0]["pressure"] * 0.007501,
+            "pressure_pa": res_vis["days"][0]["pressure"],
+            "humidity": res_vis["days"][0]["humidity"],
+            "feels_like": res_vis["days"][0]["feelslike"],
+            "wind_dir": directions[
+                int(round(float(res_vis["days"][0]["winddir"]) / 22.5)) % 16
+            ],
+            "wind_speed": res_vis["days"][0]["windspeed"],  # ? бальные цифры
+        },
+    }
+    return unmerged
+
+
 def get_merged_temp(lat: float, lon: float) -> dict:
     directions = [
         "N",
@@ -63,39 +125,53 @@ def get_merged_temp(lat: float, lon: float) -> dict:
     res_open = get_open_weather(lat, lon)
     res_vis = get_vis_crossing_weather(lat, lon)
     merged = {
-        "Yandex": {
-            "temp": res_yan["fact"]["temp"],
+        "Weather": {
+            "temp": (
+                (res_vis["days"][0]["tempmax"] + res_vis["days"][0]["tempmin"]) / 2
+                + (res_open["main"]["temp_min"] + res_open["main"]["temp_max"]) / 2
+                + res_yan["fact"]["temp"]
+            )
+            / 3,
             "condition": "smth",
-            "pressure_mm": res_yan["fact"]["pressure_mm"],
-            "pressure_pa": res_yan["fact"]["pressure_pa"],
-            "humidity": res_yan["fact"]["humidity"],
-            "feels_like": res_yan["fact"]["feels_like"],
-            "wind_dir": res_yan["fact"]["wind_dir"],
-            "wind_speed": res_yan["fact"]["wind_speed"],
-        },
-        "OpenWeather": {
-            "temp": (res_open["main"]["temp_min"] + res_open["main"]["temp_max"]) / 2,
-            "condition": "smth",
-            "pressure_mm": res_open["main"]["pressure"] * 0.007501,
-            "pressure_pa": res_open["main"]["pressure"],
-            "humidity": res_open["main"]["humidity"],
-            "feels_like": res_open["main"]["feels_like"],
-            "wind_dir": directions[
-                int(round(float(res_open["wind"]["deg"]) / 22.5)) % 16
-            ],
-            "wind_speed": res_open["wind"]["speed"],
-        },
-        "VisualCrossing": {
-            "temp": (res_vis["days"][0]["tempmax"] + res_vis["days"][0]["tempmin"]) / 2,
-            "condition": "smth",
-            "pressure_mm": res_vis["days"][0]["pressure"] * 0.007501,
-            "pressure_pa": res_vis["days"][0]["pressure"],
-            "humidity": res_vis["days"][0]["humidity"],
-            "feels_like": res_vis["days"][0]["feelslike"],
-            "wind_dir": directions[
-                int(round(float(res_vis["days"][0]["winddir"]) / 22.5)) % 16
-            ],
-            "wind_speed": res_vis["days"][0]["winddir"],  # ? бальные цифры
+            "pressure_mm": (
+                res_vis["days"][0]["pressure"] * 0.007501
+                + res_open["main"]["pressure"] * 0.007501
+                + res_yan["fact"]["pressure_mm"]
+            )
+            / 3,
+            "pressure_pa": (
+                res_vis["days"][0]["pressure"]
+                + res_open["main"]["pressure"]
+                + res_yan["fact"]["pressure_pa"]
+            )
+            / 3,
+            "humidity": (
+                res_vis["days"][0]["humidity"]
+                + res_open["main"]["humidity"]
+                + res_yan["fact"]["humidity"]
+            )
+            / 3,
+            "feels_like": (
+                res_vis["days"][0]["feelslike"]
+                + res_open["main"]["feels_like"]
+                + res_yan["fact"]["feels_like"]
+            )
+            / 3,
+            "wind_dir": Counter(
+                [
+                    directions[
+                        int(round(float(res_vis["days"][0]["winddir"]) / 22.5)) % 16
+                    ]
+                    + directions[int(round(float(res_open["wind"]["deg"]) / 22.5)) % 16]
+                    + res_yan["fact"]["wind_dir"].upper()
+                ]
+            ).most_common(1),
+            "wind_speed": (
+                res_vis["days"][0]["windspeed"]
+                + res_open["wind"]["speed"]
+                + res_yan["fact"]["wind_speed"]
+            )
+            / 3,
         },
     }
 
